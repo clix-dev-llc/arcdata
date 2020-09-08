redcross/dcsops

Unsqueel: replace all uses of `includes` and `joins`
This replaces Squeel `.includes{}` and `.joins{}` calls with vanilla AR
versions.

app/admin/incidents/notifications/roles.rb
@@ -16,7 +16,7 @@ ActiveAdmin.register Incidents::Notifications::Role, as: 'Notification Role' do
       safe_join(r.positions.map(&:name) + r.shifts.map(&:name), tag(:br))
     }
     column(:counties) { |r|
-      safe_join(r.role_scopes.includes{territory}.map{|rs| rs.territory ? "Territory: #{rs.territory.name}" : "County: #{rs.value}" }, tag(:br))
+      safe_join(r.role_scopes.includes(:territory).map{|rs| rs.territory ? "Territory: #{rs.territory.name}" : "County: #{rs.value}" }, tag(:br))
     }
     actions
   end
@@ -63,7 +63,7 @@ ActiveAdmin.register Incidents::Notifications::Role, as: 'Notification Role' do
       end
       column do
         panel "Triggers" do
-          table_for role.triggers.joins{event}.order('event.ordinal') do
+          table_for role.triggers.joins(:event).order('event.ordinal') do
             column :event
             column(:template) { |t| t.humanized_template }
             column :use_sms
@@ -99,7 +99,7 @@ ActiveAdmin.register Incidents::Notifications::Role, as: 'Notification Role' do
   controller do
 
     def collection
-      @col ||= super.includes{[positions,shifts.shift_groups, shifts.county]}
+      @col ||= super.includes(:positions, shifts: [:shift_groups, :county])
     end
 
     def resource_params

app/admin/scheduler/dispatch_configs.rb
@@ -25,7 +25,7 @@ ActiveAdmin.register Scheduler::DispatchConfig, as: 'Dispatch Configs' do
       f.input :county, collection: f.object.chapter.try(:counties), input_html: {disabled: !allow_edit_names?}
       f.input :is_active, input_html: {disabled: !allow_edit_names?}
       if f.object.chapter
-        shifts = Scheduler::Shift.for_chapter(f.object.chapter).joins{county}.order('county.name', :name).includes{[shift_groups, county]}
+        shifts = Scheduler::Shift.for_chapter(f.object.chapter).joins(:county).order('county.name', :name).includes(:shift_groups, :county)
         f.input :shift_first, collection: shifts
         f.input :shift_second, collection: shifts
         f.input :shift_third, collection: shifts

app/controllers/scheduler/home_controller.rb
@@ -12,7 +12,7 @@ class Scheduler::HomeController < Scheduler::BaseController
   private
   helper_method :shifts_available_for_month
   def shifts_available_for_month(month)
-    @shifts ||= Scheduler::Shift.for_chapter(current_chapter).includes{shift_groups}.can_be_taken_by(current_person)
+    @shifts ||= Scheduler::Shift.for_chapter(current_chapter).includes(:shift_groups).can_be_taken_by(current_person)
 
     Scheduler::Shift.count_shifts_available_for_month(@shifts, month)
   end
@@ -40,7 +40,7 @@ class Scheduler::HomeController < Scheduler::BaseController
 
   helper_method :responses
   def responses
-    @_responses ||= Incidents::ResponderAssignment.where(person_id: current_person).joins{incident}.where{incident.status == 'closed'}.includes{incident}.order('incidents_incidents.date desc').first(5)
+    @_responses ||= Incidents::ResponderAssignment.where(person_id: current_person).joins(:incident).where{incident.status == 'closed'}.includes(:incident).order('incidents_incidents.date desc').first(5)
   end
 
   helper_method :days_of_week, :shift_times, :current_person

app/helpers/scheduler/home_helper.rb
@@ -14,7 +14,7 @@ module Scheduler::HomeHelper
 
   def current_shifts_tree(counties, groups=nil)
     counties = counties.to_a
-    groups ||= Scheduler::ShiftGroup.current_groups_for_chapter(current_person.chapter, current_chapter.time_zone.now, Scheduler::ShiftGroup.includes{shifts})
+    groups ||= Scheduler::ShiftGroup.current_groups_for_chapter(current_person.chapter, current_chapter.time_zone.now, Scheduler::ShiftGroup.includes(:shifts))
     groups_by_id = groups.group_by(&:id)
     groups_by_shift = groups.reduce({}){|hash, group| group.shift_ids.each{|id| hash[id] = group}; hash }
 

app/jobs/incidents/weekly_report_job.rb
@@ -51,7 +51,7 @@ class Incidents::WeeklyReportJob
     Incidents::ReportSubscription.for_type('report')
                                  .for_scope(scope)
                                  .to_send_on(current_send_date)
-                                 .includes{person.chapter}
+                                 .includes(person: :chapter)
                                  .with_active_person
                                  .readonly(false)
   end

app/models/incidents/deployment.rb
@@ -4,7 +4,7 @@ class Incidents::Deployment < ApplicationRecord
 
   validates_presence_of :person, :disaster
 
-  scope :for_chapter, ->(chapters) { joins{person}.where{person.chapter_id.in chapters}}
+  scope :for_chapter, ->(chapters) { joins(:person).where{person.chapter_id.in chapters}}
   scope :seen_since, ->(date) { where{date_last_seen >= date} }
 
   def self.for_person person

app/models/scheduler/scheduler_service.rb
@@ -7,7 +7,7 @@ class Scheduler::SchedulerService
 
   def scheduled_responders(time: chapter.time_zone.now, limit: nil, areas: nil, exclude: [], shifts: nil, dispatch_console: false)
     groups = Scheduler::ShiftGroup.current_groups_for_chapter(chapter, time)
-    assignments = Scheduler::ShiftAssignment.joins{[shift]}.preload{[shift, person]}.for_active_groups(groups)
+    assignments = Scheduler::ShiftAssignment.joins(:shift).preload{[shift, person]}.for_active_groups(groups)
                   .where{person_id.not_in(exclude)}.limit(limit)
     if areas.present?
       assignments = assignments.where{shift.county_id.in areas}
@@ -27,7 +27,7 @@ class Scheduler::SchedulerService
     offset = time.seconds_since_midnight
     period = (offset >= chapter.scheduler_flex_day_start && offset < chapter.scheduler_flex_night_start) ? 'day' : 'night'
 
-    schedules = Scheduler::FlexSchedule.available_at(dow, period).joins{person}.where{person.chapter_id == my{chapter}}.preload{person}
+    schedules = Scheduler::FlexSchedule.available_at(dow, period).joins(:person).where{person.chapter_id == my{chapter}}.preload{person}
     if areas.present?
       schedules = schedules.for_county(areas)
     end
@@ -43,7 +43,7 @@ class Scheduler::SchedulerService
     if config
       groups = Scheduler::ShiftGroup.current_groups_for_chapter(chapter, time)
       shifts = config.shift_list
-      assignments = Scheduler::ShiftAssignment.for_active_groups(groups).for_shifts(shifts).includes{shift}.sort_by{|sa| shifts.index(sa.shift) }
+      assignments = Scheduler::ShiftAssignment.for_active_groups(groups).for_shifts(shifts).includes(:shift).sort_by{|sa| shifts.index(sa.shift) }
       backup = config.backup_list
       {present: true, assignments: assignments, backup: backup}
     else

clix-dev-llc/arcdata

app/admin/incidents/notifications/roles.rb
@@ -16,7 +16,7 @@ ActiveAdmin.register Incidents::Notifications::Role, as: 'Notification Role' do
       safe_join(r.positions.map(&:name) + r.shifts.map(&:name), tag(:br))
     }
     column(:shift_territories) { |r|
-      safe_join(r.role_scopes.includes{response_territory}.map{|rs| rs.response_territory ? "Response Territory: #{rs.response_territory.name}" : "Shift Territory: #{rs.value}" }, tag(:br))
+      safe_join(r.role_scopes.includes(:response_territory).map{|rs| rs.response_territory ? "Response Territory: #{rs.response_territory.name}" : "Shift Territory: #{rs.value}" }, tag(:br))
     }
     actions
   end
@@ -70,7 +70,7 @@ ActiveAdmin.register Incidents::Notifications::Role, as: 'Notification Role' do
       end
       column do
         panel "Triggers" do
-          table_for role.triggers.joins{event}.order('event.ordinal') do
+          table_for role.triggers.joins(:event).order('event.ordinal') do
             column :event
             column(:template) { |t| t.humanized_template }
             column :use_sms
@@ -106,7 +106,7 @@ ActiveAdmin.register Incidents::Notifications::Role, as: 'Notification Role' do
   controller do
 
     def collection
-      @col ||= super.includes{[positions,shifts.shift_times, shifts.shift_territory]}
+      @col ||= super.includes(:positions, shifts: [:shift_times, :shift_territory])
     end
 
     def resource_params

app/admin/scheduler/dispatch_configs.rb
@@ -25,7 +25,7 @@ ActiveAdmin.register Scheduler::DispatchConfig, as: 'Dispatch Configs' do
       f.input :shift_territory, collection: f.object.region.try(:shift_territories), input_html: {disabled: !allow_edit_names?}
       f.input :is_active, input_html: {disabled: !allow_edit_names?}
       if f.object.region
-        shifts = Scheduler::Shift.for_region(f.object.region).joins{shift_territory}.order('shift_territory.name', :name).includes{[shift_times, shift_territory]}
+        shifts = Scheduler::Shift.for_region(f.object.region).joins(:shift_territory).order('shift_territory.name', :name).includes(:shift_times, :shift_territory)
         f.input :shift_first, collection: shifts
         f.input :shift_second, collection: shifts
         f.input :shift_third, collection: shifts

app/controllers/scheduler/home_controller.rb
@@ -12,7 +12,7 @@ class Scheduler::HomeController < Scheduler::BaseController
   private
   helper_method :shifts_available_for_month
   def shifts_available_for_month(month)
-    @shifts ||= Scheduler::Shift.for_region(current_region).includes{shift_times}.can_be_taken_by(current_person)
+    @shifts ||= Scheduler::Shift.for_region(current_region).includes(:shift_times).can_be_taken_by(current_person)
 
     Scheduler::Shift.count_shifts_available_for_month(@shifts, month)
   end
@@ -40,7 +40,7 @@ class Scheduler::HomeController < Scheduler::BaseController
 
   helper_method :responses
   def responses
-    @_responses ||= Incidents::ResponderAssignment.where(person_id: current_person).joins{incident}.where{incident.status == 'closed'}.includes{incident}.order('incidents_incidents.date desc').first(5)
+    @_responses ||= Incidents::ResponderAssignment.where(person_id: current_person).joins(:incident).where{incident.status == 'closed'}.includes(:incident).order('incidents_incidents.date desc').first(5)
   end
 
   helper_method :days_of_week, :shift_times, :current_person

app/helpers/scheduler/home_helper.rb
@@ -14,7 +14,7 @@ module Scheduler::HomeHelper
 
   def current_shifts_tree(shift_territories, groups=nil)
     shift_territories = shift_territories.to_a
-    groups ||= Scheduler::ShiftTime.current_groups_for_region(current_person.region, current_region.time_zone.now, Scheduler::ShiftTime.includes{shifts})
+    groups ||= Scheduler::ShiftTime.current_groups_for_region(current_person.region, current_region.time_zone.now, Scheduler::ShiftTime.includes(:shifts))
     groups_by_id = groups.group_by(&:id)
     groups_by_shift = groups.reduce({}){|hash, group| group.shift_ids.each{|id| hash[id] = group}; hash }
 

app/jobs/incidents/weekly_report_job.rb
@@ -51,7 +51,7 @@ class Incidents::WeeklyReportJob
     Incidents::ReportSubscription.for_type('report')
                                  .for_scope(scope)
                                  .to_send_on(current_send_date)
-                                 .includes{person.region}
+                                 .includes(person: :region)
                                  .with_active_person
                                  .readonly(false)
   end

app/models/incidents/deployment.rb
@@ -4,7 +4,7 @@ class Incidents::Deployment < ApplicationRecord
 
   validates_presence_of :person, :disaster
 
-  scope :for_region, ->(regions) { joins{person}.where{person.region_id.in regions}}
+  scope :for_region, ->(regions) { joins(:person).where{person.region_id.in regions}}
   scope :seen_since, ->(date) { where{date_last_seen >= date} }
 
   def self.for_person person

app/models/scheduler/scheduler_service.rb
@@ -7,7 +7,7 @@ class Scheduler::SchedulerService
 
   def scheduled_responders(time: region.time_zone.now, limit: nil, shift_territories: nil, exclude: [], shifts: nil, dispatch_console: false)
     groups = Scheduler::ShiftTime.current_groups_for_region(region, time)
-    assignments = Scheduler::ShiftAssignment.joins{[shift]}.preload{[shift, person]}.for_active_groups(groups)
+    assignments = Scheduler::ShiftAssignment.joins(:shift).preload{[shift, person]}.for_active_groups(groups)
                   .where{person_id.not_in(exclude)}.limit(limit)
     if shift_territories.present?
       assignments = assignments.where{shift.shift_territory_id.in shift_territories}
@@ -27,7 +27,7 @@ class Scheduler::SchedulerService
     offset = time.seconds_since_midnight
     period = (offset >= region.scheduler_flex_day_start && offset < region.scheduler_flex_night_start) ? 'day' : 'night'
 
-    schedules = Scheduler::FlexSchedule.available_at(dow, period).joins{person}.where{person.region_id == my{region}}.preload{person}
+    schedules = Scheduler::FlexSchedule.available_at(dow, period).joins(:person).where{person.region_id == my{region}}.preload{person}
     if shift_territories.present?
       schedules = schedules.for_shift_territory(shift_territories)
     end
@@ -43,7 +43,7 @@ class Scheduler::SchedulerService
     if config
       groups = Scheduler::ShiftTime.current_groups_for_region(region, time)
       shifts = config.shift_list
-      assignments = Scheduler::ShiftAssignment.for_active_groups(groups).for_shifts(shifts).includes{shift}.sort_by{|sa| shifts.index(sa.shift) }
+      assignments = Scheduler::ShiftAssignment.for_active_groups(groups).for_shifts(shifts).includes(:shift).sort_by{|sa| shifts.index(sa.shift) }
       backup = config.backup_list
       {present: true, assignments: assignments, backup: backup}
     else