redcross/dcsops

Unsqueel: replace all uses of `select`
This replaces Squeel `.select{}` calls with vanilla AR versions.

This was a little tricky to search for since `select` is also a heavily
used method on Enumerable, but I think I got all of the Squeel uses.

app/models/scheduler/shift.rb
@@ -121,8 +121,12 @@ class Scheduler::Shift < ApplicationRecord
       end
     end
 
-    arr = Scheduler::ShiftAssignment.where(shift_id: shifts.to_a, date: month..(month.at_end_of_month))
-                              .select{[count(id).as(:count), shift_id, shift_group_id, date]}.group{[shift_id, shift_group_id, date]}.reduce(arr) do |hash, ass|
+    arr = Scheduler::ShiftAssignment
+      .where(shift_id: shifts.to_a, date: month..(month.at_end_of_month))
+      .select('COUNT(id) AS count, shift_id, shift_group_id, date')
+      .group{[shift_id, shift_group_id, date]}
+      .reduce(arr) do |hash, ass|
+
       shift = shifts_by_id[ass.shift_id]
       group = groups_by_id[ass.shift_group_id]
       if shift.nil? || group.nil?

clix-dev-llc/arcdata

app/models/scheduler/shift.rb
@@ -121,8 +121,11 @@ class Scheduler::Shift < ApplicationRecord
       end
     end
 
-    arr = Scheduler::ShiftAssignment.where(shift_id: shifts.to_a, date: month..(month.at_end_of_month))
-                              .select{[count(id).as(:count), shift_id, shift_time_id, date]}.group{[shift_id, shift_time_id, date]}.reduce(arr) do |hash, ass|
+    arr = Scheduler::ShiftAssignment
+      .where(shift_id: shifts.to_a, date: month..(month.at_end_of_month))
+      .select('COUNT(id) AS count, shift_id, shift_time_id, date')
+      .group{[shift_id, shift_time_id, date]}
+      .reduce(arr) do |hash, ass|
       shift = shifts_by_id[ass.shift_id]
       group = groups_by_id[ass.shift_time_id]
       if shift.nil? || group.nil?