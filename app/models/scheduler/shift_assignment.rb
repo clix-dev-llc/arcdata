redcross/dcsops

Rails 5 upgrade: AR query generation change
This is used in a lot of specs, which is why it got individual
attention.

I'm not completely sure why this worked before (presumably) but isn't
now under Rails 5. It's not even part of the Squeel migration. But, it
was generating a query like this:

    SELECT  1 AS one
    FROM "scheduler_shift_assignments"
    LEFT OUTER JOIN "scheduler_shifts"
      ON "scheduler_shifts"."id" = "scheduler_shift_assignments"."shift_id"
    WHERE "shift"."exclusive" = $1
      AND "scheduler_shift_assignments"."shift_group_id" = $2
      AND "scheduler_shift_assignments"."date" = $3
      AND "scheduler_shift_assignments"."person_id" = $4
    LIMIT $5

The first where clause is mis-specified (`shift` when it should be
`scheduler_shifts`); for some reason Rails is not using the correct
table name, which it should know based on the inclusion of `shift`
which is an association on this model.

I spent a little time trying to chase down why it was doing that,
including trying the `.references(:shift)` to help it out, but didn't
have any luck so I went with this brute-force method of just fully
specifying the condition.

app/models/scheduler/shift_assignment.rb
@@ -39,7 +39,7 @@ class Scheduler::ShiftAssignment < ApplicationRecord
     def validate(record)
       return false unless record.shift and record.date and record.person
       return true unless record.shift.exclusive
-      assignments = Scheduler::ShiftAssignment.includes(:shift).where(shift: {exclusive: true}, shift_group_id: record.shift_group, date: record.date, person_id: record.person)
+      assignments = Scheduler::ShiftAssignment.includes(:shift).where(scheduler_shifts: {exclusive: true}, shift_group_id: record.shift_group, date: record.date, person_id: record.person)
       if record.id
         assignments = assignments.where("scheduler_shift_assignments.id <> ?", record.id)
       end

clix-dev-llc/arcdata

app/models/scheduler/shift_assignment.rb
@@ -39,7 +39,7 @@ class Scheduler::ShiftAssignment < ApplicationRecord
     def validate(record)
       return false unless record.shift and record.date and record.person
       return true unless record.shift.exclusive
-      assignments = Scheduler::ShiftAssignment.includes(:shift).where(shift: {exclusive: true}, shift_time_id: record.shift_time, date: record.date, person_id: record.person)
+      assignments = Scheduler::ShiftAssignment.includes(:shift).where(scheduler_shifts: {exclusive: true}, shift_time_id: record.shift_time, date: record.date, person_id: record.person)
       if record.id
         assignments = assignments.where("scheduler_shift_assignments.id <> ?", record.id)
       end