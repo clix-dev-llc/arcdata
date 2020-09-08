redcross/dcsops

Unsqueel: start on Incidents::Ability model
I stopped here due to exploding complexity related to the heavye use of
Core::SerializedColumn, which comes from arcdata_core and has its own
squeel dependencies plus tight dependencies on internal ActiveRecord
machinery which has changed under Rails 5 (related to database field
type casting).

app/models/incidents/ability.rb
@@ -6,7 +6,7 @@ class Incidents::Ability
 
   def initialize(person)
     @person = person
-    @chapter_scope = [@person.chapter_id] + Roster::Chapter.with_incidents_delegate_chapter_value(@person.chapter_id).ids
+    @chapter_scope = [@person.chapter_id] + Roster::Chapter.with_incidents_delegate_chapter_value(@person.chapter_id).pluck(:id)
     is_admin = person.has_role 'incidents_admin'
 
     scopes

clix-dev-llc/arcdata

app/models/incidents/ability.rb
@@ -6,7 +6,7 @@ class Incidents::Ability
 
   def initialize(person)
     @person = person
-    @region_scope = [@person.region_id] + Roster::Region.with_incidents_delegate_region_value(@person.region_id).ids
+    @region_scope = [@person.region_id] + Roster::Region.with_incidents_delegate_region_value(@person.region_id).pluck(:id)
     is_admin = person.has_capability 'incidents_admin'
     region_admin = person.has_capability 'region_admin'
 