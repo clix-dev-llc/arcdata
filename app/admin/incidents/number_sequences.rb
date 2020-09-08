redcross/dcsops

Unsqueel: replace all uses of `preload`
This replaces Squeel `.preload{}` calls with vanilla AR versions.

app/admin/incidents/number_sequences.rb
@@ -17,7 +17,7 @@ ActiveAdmin.register Incidents::NumberSequence, as: 'Number Sequence' do
 
   controller do
     def collection
-      @col ||= super.preload{chapters}
+      @col ||= super.preload(:chapters)
     end
   end
 end

app/controllers/incidents/incidents_list_controller.rb
@@ -36,7 +36,7 @@ class Incidents::IncidentsListController < Incidents::BaseController
 
   def collection
     @_incidents ||= begin
-      scope = apply_scopes(super).order(date: :desc, incident_number: :desc)#.preload{[area, dat_incident, team_lead.person]}
+      scope = apply_scopes(super).order(date: :desc, incident_number: :desc)#.preload(:area, :dat_incident, team_lead: :person)
       scope
     end
   end

app/controllers/scheduler/flex_schedules_controller.rb
@@ -31,6 +31,6 @@ class Scheduler::FlexSchedulesController < Scheduler::BaseController
     end
 
     def collection
-      @collection ||= apply_scopes(super).uniq.preload{[person.positions, person.counties, person.home_phone_carrier, person.work_phone_carrier, person.alternate_phone_carrier, person.cell_phone_carrier, person.sms_phone_carrier]}
+      @collection ||= apply_scopes(super).uniq.preload(person: [:positions, :counties, :home_phone_carrier, :work_phone_carrier, :alternate_phone_carrier, :cell_phone_carrier, :sms_phone_carrier])
     end
 end

app/mailers/incidents/report_mailer.rb
@@ -67,7 +67,7 @@ private
     ignore = scope.report_dro_ignore_array
     chapters = scope.all_chapters
     Incidents::Deployment.for_chapter(chapters).seen_since(@date_range.first)
-                          .preload{[disaster, person.counties]}
+                          .preload(:disaster, person: :counties)
                           .joins(:disaster)
                           .where.not(disaster: { dr_number: ignore })
                           .order(date_first_seen: :desc)

app/models/incidents/responders_service.rb
@@ -29,7 +29,7 @@ class Incidents::RespondersService
   end
 
   def scheduled_responders
-    @scheduled ||= service.scheduled_responders(areas: scheduled_areas, exclude: exclude_scheduled, dispatch_console: true).preload{person.positions}
+    @scheduled ||= service.scheduled_responders(areas: scheduled_areas, exclude: exclude_scheduled, dispatch_console: true).preload(person: :positions)
   end
 
   def flex_responders

app/models/scheduler/scheduler_service.rb
@@ -7,7 +7,7 @@ class Scheduler::SchedulerService
 
   def scheduled_responders(time: chapter.time_zone.now, limit: nil, areas: nil, exclude: [], shifts: nil, dispatch_console: false)
     groups = Scheduler::ShiftGroup.current_groups_for_chapter(chapter, time)
-    assignments = Scheduler::ShiftAssignment.joins(:shift).preload{[shift, person]}.for_active_groups(groups)
+    assignments = Scheduler::ShiftAssignment.joins(:shift).preload(:shift, :person).for_active_groups(groups)
                   .where.not(person_id: exclude).limit(limit)
     if areas.present?
       assignments = assignments.where(shift: { county_id: areas })
@@ -27,7 +27,7 @@ class Scheduler::SchedulerService
     offset = time.seconds_since_midnight
     period = (offset >= chapter.scheduler_flex_day_start && offset < chapter.scheduler_flex_night_start) ? 'day' : 'night'
 
-    schedules = Scheduler::FlexSchedule.available_at(dow, period).joins(:person).where(person: { chapter_id: chapter }).preload{person}
+    schedules = Scheduler::FlexSchedule.available_at(dow, period).joins(:person).where(person: { chapter_id: chapter }).preload(:person)
     if areas.present?
       schedules = schedules.for_county(areas)
     end
@@ -35,7 +35,7 @@ class Scheduler::SchedulerService
       schedules = schedules.by_distance_from origin
     end
 
-    people = schedules.where.not(id: exclude).preload{[person.positions, person.cell_phone_carrier, person.work_phone_carrier, person.home_phone_carrier, person.alternate_phone_carrier, person.sms_phone_carrier]}.limit(limit).to_a.uniq{|s| s.id }
+    people = schedules.where.not(id: exclude).preload(person: [:positions, :cell_phone_carrier, :work_phone_carrier, :home_phone_carrier, :alternate_phone_carrier, :sms_phone_carrier]).limit(limit).to_a.uniq{|s| s.id }
   end
 
   def dispatch_assignments(time: chapter.time_zone.now, territory: )

app/models/scheduler/shift_assignment.rb
@@ -138,7 +138,7 @@ class Scheduler::ShiftAssignment < ApplicationRecord
     where(:"#{type}_reminder_sent" => false)
     .joins(:notification_setting)
     .where.not(notification_setting.__send__("#{type}_advance_hours") => nil)
-    .with_active_person.for_chapter(chapter).readonly(false).preload{[notification_setting,shift_group.chapter]}
+    .with_active_person.for_chapter(chapter).readonly(false).preload(:notification_setting, shift_group: :chapter)
     .select{|ass|
       now = chapter.time_zone.now
       start = ass.local_start_time

clix-dev-llc/arcdata

app/admin/incidents/number_sequences.rb
@@ -17,7 +17,7 @@ ActiveAdmin.register Incidents::NumberSequence, as: 'Number Sequence' do
 
   controller do
     def collection
-      @col ||= super.preload{regions}
+      @col ||= super.preload(:regions)
     end
   end
 end

app/controllers/incidents/incidents_list_controller.rb
@@ -36,7 +36,7 @@ class Incidents::IncidentsListController < Incidents::BaseController
 
   def collection
     @_incidents ||= begin
-      scope = apply_scopes(super).order(date: :desc, incident_number: :desc)#.preload{[shift_territory, dat_incident, team_lead.person]}
+      scope = apply_scopes(super).order(date: :desc, incident_number: :desc)#.preload(:shift_territory, :dat_incident, team_lead: :person)
       scope
     end
   end

app/controllers/scheduler/flex_schedules_controller.rb
@@ -43,6 +43,6 @@ class Scheduler::FlexSchedulesController < Scheduler::BaseController
     end
 
     def collection
-      @collection ||= apply_scopes(super).uniq.preload{[person.positions, person.shift_territories, person.home_phone_carrier, person.work_phone_carrier, person.alternate_phone_carrier, person.cell_phone_carrier, person.sms_phone_carrier]}
+      @collection ||= apply_scopes(super).uniq.preload(person: [:positions, :shift_territories, :home_phone_carrier, :work_phone_carrier, :alternate_phone_carrier, :cell_phone_carrier, :sms_phone_carrier])
     end
 end

app/mailers/incidents/report_mailer.rb
@@ -67,7 +67,7 @@ private
     ignore = scope.report_dro_ignore_array
     regions = scope.all_regions
     Incidents::Deployment.for_region(regions).seen_since(@date_range.first)
-                          .preload{[disaster, person.shift_territories]}
+                          .preload(:disaster, person: :shift_territories)
                           .joins(:disaster)
                           .where.not(disaster: { dr_number: ignore })
                           .order(date_first_seen: :desc)

app/models/incidents/responders_service.rb
@@ -29,7 +29,7 @@ class Incidents::RespondersService
   end
 
   def scheduled_responders
-    @scheduled ||= service.scheduled_responders(shift_territories: scheduled_shift_territories, exclude: exclude_scheduled, dispatch_console: true).preload{person.positions}
+    @scheduled ||= service.scheduled_responders(shift_territories: scheduled_shift_territories, exclude: exclude_scheduled, dispatch_console: true).preload(person: :positions)
   end
 
   def flex_responders

app/models/scheduler/scheduler_service.rb
@@ -7,7 +7,7 @@ class Scheduler::SchedulerService
 
   def scheduled_responders(time: region.time_zone.now, limit: nil, shift_territories: nil, exclude: [], shifts: nil, dispatch_console: false)
     groups = Scheduler::ShiftTime.current_groups_for_region(region, time)
-    assignments = Scheduler::ShiftAssignment.joins(:shift).preload{[shift, person]}.for_active_groups(groups)
+    assignments = Scheduler::ShiftAssignment.joins(:shift).preload(:shift, :person).for_active_groups(groups)
                   .where.not(person_id: exclude).limit(limit)
     if shift_territories.present?
       assignments = assignments.where(shift: { shift_territory_id: shift_territories })
@@ -27,7 +27,7 @@ class Scheduler::SchedulerService
     offset = time.seconds_since_midnight
     period = (offset >= region.scheduler_flex_day_start && offset < region.scheduler_flex_night_start) ? 'day' : 'night'
 
-    schedules = Scheduler::FlexSchedule.available_at(dow, period).joins(:person).where(person: { region_id: region }).preload{person}
+    schedules = Scheduler::FlexSchedule.available_at(dow, period).joins(:person).where(person: { region_id: region }).preload(:person)
     if shift_territories.present?
       schedules = schedules.for_shift_territory(shift_territories)
     end
@@ -35,7 +35,7 @@ class Scheduler::SchedulerService
       schedules = schedules.by_distance_from origin
     end
 
-    people = schedules.where.not(id: exclude).preload{[person.positions, person.cell_phone_carrier, person.work_phone_carrier, person.home_phone_carrier, person.alternate_phone_carrier, person.sms_phone_carrier]}.limit(limit).to_a.uniq{|s| s.id }
+    people = schedules.where.not(id: exclude).preload(person: [:positions, :cell_phone_carrier, :work_phone_carrier, :home_phone_carrier, :alternate_phone_carrier, :sms_phone_carrier]).limit(limit).to_a.uniq{|s| s.id }
   end
 
   def dispatch_assignments(time: region.time_zone.now, response_territory: )

app/models/scheduler/shift_assignment.rb
@@ -137,7 +137,7 @@ class Scheduler::ShiftAssignment < ApplicationRecord
     where(:"#{type}_reminder_sent" => false)
     .joins(:notification_setting)
     .where.not(notification_setting.__send__("#{type}_advance_hours") => nil)
-    .with_active_person.for_region(region).readonly(false).preload{[notification_setting,shift_time.region]}
+    .with_active_person.for_region(region).readonly(false).preload(:notification_setting, shift_time: :region)
     .select{|ass|
       now = region.time_zone.now
       start = ass.local_start_time
@@ -178,7 +178,7 @@ class Scheduler::ShiftAssignment < ApplicationRecord
   end
 
   def self.ordered_shifts region
-    joins(:person).where{person.region_id==my{region}}.readonly(false).joins(:shift, :shift_time).order('shift.ordinal', 'shift_time.start_offset', 'person_id'}.preload{[shift.shift_territory, person, shift, shift_time]}
+    joins(:person).where(person: {region_id: region)).readonly(false).joins(:shift, :shift_time).order('shift.ordinal', 'shift_time.start_offset', 'person_id').preload(:person, :shift, :shift_time, shift: :shift_territory)
   end
 
   def self.todays_shifts_with_notes region