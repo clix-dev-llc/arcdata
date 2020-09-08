redcross/dcsops

Unsqueel: replace (almost) all uses of `order`
This replaces Squeel `.order{}` calls with vanilla AR order calls.

Except for one in FlexSchedule.by_distance_from which does a complex
calculation in its order clause.

app/admin/scheduler/dispatch_configs.rb
@@ -25,7 +25,7 @@ ActiveAdmin.register Scheduler::DispatchConfig, as: 'Dispatch Configs' do
       f.input :county, collection: f.object.chapter.try(:counties), input_html: {disabled: !allow_edit_names?}
       f.input :is_active, input_html: {disabled: !allow_edit_names?}
       if f.object.chapter
-        shifts = Scheduler::Shift.for_chapter(f.object.chapter).joins{county}.order{[county.name, name]}.includes{[shift_groups, county]}
+        shifts = Scheduler::Shift.for_chapter(f.object.chapter).joins{county}.order('county.name', :name).includes{[shift_groups, county]}
         f.input :shift_first, collection: shifts
         f.input :shift_second, collection: shifts
         f.input :shift_third, collection: shifts

app/controllers/admin/shifts_controller.rb
@@ -32,7 +32,7 @@ class Admin::ShiftsController < GridController
   end
 
   def collection
-    @_coll ||= super.order{[county_id, ordinal]}
+    @_coll ||= super.order(:county_id, :ordinal)
   end
 
   def chapter

app/controllers/incidents/event_logs_controller.rb
@@ -38,7 +38,7 @@ class Incidents::EventLogsController < Incidents::EditPanelController
 
   def collection
     @collection ||= begin
-      coll = super.includes{[incident, person, incident.chapter]}.order{event_time.desc}
+      coll = super.includes{[incident, person, incident.chapter]}.order(event_time :desc)
       coll = coll.page(params[:page]) if paginate?
       coll
     end

app/controllers/incidents/incidents_list_controller.rb
@@ -36,7 +36,7 @@ class Incidents::IncidentsListController < Incidents::BaseController
 
   def collection
     @_incidents ||= begin
-      scope = apply_scopes(super).order{[date.desc, incident_number.desc]}#.preload{[area, dat_incident, team_lead.person]}
+      scope = apply_scopes(super).order(date: :desc, incident_number: :desc)#.preload{[area, dat_incident, team_lead.person]}
       scope
     end
   end

app/models/incidents/cas_incident.rb
@@ -11,7 +11,7 @@ class Incidents::CasIncident < ApplicationRecord
   end
 
   def self.to_link_for_chapter chapter
-    for_chapter(chapter).joins{incident.outer}.where{(ignore_incident==false) & (incident.id == nil)}.order{incident_date.desc}
+    for_chapter(chapter).joins{incident.outer}.where{(ignore_incident==false) & (incident.id == nil)}.order(incident_date: :desc)
   end
 
   def self.open_cases

clix-dev-llc/arcdata

app/admin/scheduler/dispatch_configs.rb
@@ -25,7 +25,7 @@ ActiveAdmin.register Scheduler::DispatchConfig, as: 'Dispatch Configs' do
       f.input :shift_territory, collection: f.object.region.try(:shift_territories), input_html: {disabled: !allow_edit_names?}
       f.input :is_active, input_html: {disabled: !allow_edit_names?}
       if f.object.region
-        shifts = Scheduler::Shift.for_region(f.object.region).joins{shift_territory}.order{[shift_territory.name, name]}.includes{[shift_times, shift_territory]}
+        shifts = Scheduler::Shift.for_region(f.object.region).joins{shift_territory}.order('shift_territory.name', :name).includes{[shift_times, shift_territory]}
         f.input :shift_first, collection: shifts
         f.input :shift_second, collection: shifts
         f.input :shift_third, collection: shifts

app/controllers/admin/shifts_controller.rb
@@ -32,7 +32,7 @@ class Admin::ShiftsController < GridController
   end
 
   def collection
-    @_coll ||= super.order{[shift_territory_id, ordinal]}
+    @_coll ||= super.order(:shift_territory_id, :ordinal)
   end
 
   def region

app/controllers/incidents/event_logs_controller.rb
@@ -38,7 +38,7 @@ class Incidents::EventLogsController < Incidents::EditPanelController
 
   def collection
     @collection ||= begin
-      coll = super.includes{[incident, person, incident.region]}.order{event_time.desc}
+      coll = super.includes{[incident, person, incident.region]}.order(event_time :desc)
       coll = coll.page(params[:page]) if paginate?
       coll
     end

app/controllers/incidents/incidents_list_controller.rb
@@ -36,7 +36,7 @@ class Incidents::IncidentsListController < Incidents::BaseController
 
   def collection
     @_incidents ||= begin
-      scope = apply_scopes(super).order{[date.desc, incident_number.desc]}#.preload{[shift_territory_incident, team_lead.person]}
+      scope = apply_scopes(super).order(date: :desc, incident_number: :desc)#.preload{[shift_territory, dat_incident, team_lead.person]}
       scope
     end
   end

app/models/incidents/cas_incident.rb
@@ -11,7 +11,7 @@ class Incidents::CasIncident < ApplicationRecord
   end
 
   def self.to_link_for_region region
-    for_region(region).joins{incident.outer}.where{(ignore_incident==false) & (incident.id == nil)}.order{incident_date.desc}
+    for_region(region).joins{incident.outer}.where{(ignore_incident==false) & (incident.id == nil)}.order(incident_date: :desc)
   end
 
   def self.open_cases