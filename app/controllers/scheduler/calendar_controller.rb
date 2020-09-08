redcross/dcsops

Unsqueel: replace (almost) all uses of `where`
This is a big one. It replaces most Squeel `.where{}` calls with
vanilla AR where calls.

This was a largely mechanical operation and so I don't have 100%
confidence that the resulting code is fully correct. In particular, I
suspect there are places where the predicate of the where clause was
something special to Squeel but AR will not know what to do with. An
example from `incidents_list_helper.rb`:

The original Squeel call was:

`(incident_id == incidents_incidents.id)`

…which I mechanically converted to:

`incident_id: incidents_incidents.id`

…however I do not think `incidents_incidents.id` is a valid method call
in the context of that file. And there are a few other similar
instances where the predicates were more complex than a simple value or
booleans/nil.

Unfortunately time constraints are such that I couldn't puzzle out the
correct equivalent AR-based queries to replace these cases, so I left
them in place where they'll (hopefully) cause a visible failure at the
time of use, and can be given more attention and remedied at that point.

In addition to those unknown cases, there are a couple others (which
are noted with `NOTE:`s) where I suspect a bug or otherwise incorrect
usage in the original Squeel calls.

In these cases, rather than taking a potentially-bad guess at the
original intent, I've inserted an exception raise. This will force any
future callers of that code to take a closer look at them and figure
out the true intended functionality.

app/controllers/scheduler/calendar_controller.rb
@@ -102,7 +102,7 @@ class Scheduler::CalendarController < Scheduler::BaseController
     @_show_categories ||= if params[:categories].present?
       Array(params[:categories]).select(&:present?).map(&:to_i)
     else
-      Scheduler::ShiftCategory.for_chapter(current_chapter).where{show == true}.ids
+      Scheduler::ShiftCategory.for_chapter(current_chapter).where(show: true).ids
     end
   end
 

app/controllers/scheduler/shift_assignments_controller.rb
@@ -10,9 +10,9 @@ class Scheduler::ShiftAssignmentsController < Scheduler::BaseController
     when 'mine'
       if controller.params[:person_id]
         controller.authorize! :read, Scheduler::ShiftAssignment.new(person_id: controller.params[:person_id])
-        scope.where{person_id == my{controller.params[:person_id]}}
+        scope.where(person_id: controller.params[:person_id])
       else
-        scope.where{person_id == my{controller.current_user.id}}
+        scope.where(person_id: controller.current_user.id)
       end
     when 'all'
       controller.authorize! :read_all_shifts, Scheduler::ShiftAssignment
@@ -65,7 +65,8 @@ class Scheduler::ShiftAssignmentsController < Scheduler::BaseController
 
   def collection
     @shift_assignments ||= apply_scopes(super).order(:date).left_outer_joins(:person)
-                  .where{(person.chapter_id == my{current_chapter}) & (date <= my{current_chapter.time_zone.today + 30})}
+                  .where(person: { chapter_id: current_chapter})
+                  .where('date <= ?', current_chapter.time_zone.today + 30)
                   .includes(:person, shift_group: :chapter, shift: :county, person: [:counties, :chapter]).uniq
   end
 

app/mailers/scheduler/directline_mailer.rb
@@ -45,7 +45,9 @@ class Scheduler::DirectlineMailer < ActionMailer::Base
     all_groups = Scheduler::ShiftGroup.for_chapter(chapter).order(:start_offset).to_a
     daily_groups = all_groups.select{ |grp| grp.period == 'daily' && dispatch_group_ids.include?(grp.id) }
     
-    all_assignments = Scheduler::ShiftAssignment.where{shift_id.in(dispatch_shifts)}.normalized_date_on_or_after(start_date).where{date <= end_date}.group_by{|sa| [sa.date, sa.shift_id, sa.shift_group_id]}
+    all_assignments = Scheduler::ShiftAssignment.where(shift_id: dispatch_shifts)
+      .normalized_date_on_or_after(start_date)
+      .where('date <= ?', end_date).group_by{|sa| [sa.date, sa.shift_id, sa.shift_group_id]}
 
     (start_date..end_date).each do |date|
       daily_groups.each do |daily_group|

app/models/scheduler/notification_setting.rb
@@ -5,25 +5,25 @@ class Scheduler::NotificationSetting < ApplicationRecord
 
   def self.needs_daily_email(chapter); needs_daily(chapter, :email); end
   def self.needs_daily_sms(chapter);   needs_daily(chapter, :sms); end
-  def self.needs_daily_swap(chapter);  for_chapter(chapter).with_active_person.where{email_all_swaps_daily == true}; end
+  def self.needs_daily_swap(chapter);  for_chapter(chapter).with_active_person.where(email_all_swaps_daily: true); end
 
   def self.needs_daily chapter, method
     now = chapter.time_zone.now
     midnight = now.at_beginning_of_day
     offset = now.seconds_since_midnight
-    for_chapter(chapter).with_active_person.where{(__send__(:"#{method}_all_shifts_at") != nil) & 
-                                                  (__send__(:"#{method}_all_shifts_at") <= offset) & 
-                                                  ( (__send__(:"last_all_shifts_#{method}") == nil) | 
-                                                    (__send__(:"last_all_shifts_#{method}") < midnight)
-                                                  )}.readonly(false)
+    for_chapter(chapter).with_active_person
+      .where("#{__send__(:"#{method}_all_shifts_at")} IS NOT NULL")
+      .where("#{__send__(:"#{method}_all_shifts_at")} <= ?", offset)
+      .where("#{__send__(:"last_all_shifts_#{method}")} IS NULL OR #{__send__(:"last_all_shifts_#{method}")} < ?", midnight)
+      .readonly(false)
   end
 
   def self.with_active_person
-    joins(:person).where{person.vc_is_active == true}
+    joins(:person).where(person: { vc_is_active: true })
   end
 
   def self.for_chapter chapter
-    joins(:person).where{person.chapter_id == chapter.id}
+    joins(:person).where(person: { chapter_id: chapter.id })
   end
 
   before_create :set_calendar_api_token
@@ -36,7 +36,9 @@ class Scheduler::NotificationSetting < ApplicationRecord
   end
 
   def self.admins_to_notify_swap(shift_assignment, ignore=[])
-    Roster::Person.joins(:notification_setting).in_county(shift_assignment.shift.county).where{(notification_setting.email_all_swaps == true) & (id.not_in ignore)}.to_a
+    Roster::Person.joins(:notification_setting).in_county(shift_assignment.shift.county)
+      .where(notification_setting: { email_all_swaps: true })
+      .where.not(id: ignore).to_a
   end
 
   def allow_sms_at? time

app/models/scheduler/shift.rb
@@ -72,20 +72,23 @@ class Scheduler::Shift < ApplicationRecord
   end
 
   scope :for_chapter, -> chapter {
-    joins(:county).where{county.chapter_id == chapter}
+    joins(:county).where(county: { chapter_id: chapter })
   }
   scope :active_on_day, -> date {
     #Todo: check day of week here
-    where{((shift_begins == nil) | (shift_begins <= date) & ((shift_ends == nil) | (shift_ends > date)))}
+    where('shift_begins IS NULL OR shift_begins <= ? AND (shift_ends IS NULL OR shift_ends > ?)', date, date)
   }
   scope :for_counties, -> counties {
-    where{county_id.in(counties)}
+    where(county_id: counties)
   }
   scope :can_be_taken_by, -> person {
-    where{((ignore_county == true) | county_id.in(person.county_ids))}.joins(:positions).where{positions.id.in(person.position_ids)}.uniq
+    where(ignore_county: true).or(where(county_id: person.county_ids))
+      .joins(:positions)
+      .where(positions: { id: person.position_ids })
+      .uniq
   }
   scope :for_groups, -> groups {
-    joins(:shift_groups).where{shift_groups.id.in groups}
+    joins(:shift_groups).where(shift_groups: { id: groups })
   }
 
   def can_be_taken_by?(person)
@@ -118,7 +121,7 @@ class Scheduler::Shift < ApplicationRecord
       end
     end
 
-    arr = Scheduler::ShiftAssignment.where{shift_id.in(shifts.to_a) & date.in(month..(month.at_end_of_month))}
+    arr = Scheduler::ShiftAssignment.where(shift_id: shifts.to_a, date: month..(month.at_end_of_month))
                               .select{[count(id).as(:count), shift_id, shift_group_id, date]}.group{[shift_id, shift_group_id, date]}.reduce(arr) do |hash, ass|
       shift = shifts_by_id[ass.shift_id]
       group = groups_by_id[ass.shift_group_id]

clix-dev-llc/arcdata

app/controllers/scheduler/calendar_controller.rb
@@ -102,7 +102,7 @@ class Scheduler::CalendarController < Scheduler::BaseController
     @_show_categories ||= if params[:categories].present?
       Array(params[:categories]).select(&:present?).map(&:to_i)
     else
-      Scheduler::ShiftCategory.for_region(current_region).where{show == true}.ids
+      Scheduler::ShiftCategory.for_region(current_region).where(show: true).ids
     end
   end
 

app/controllers/scheduler/shift_assignments_controller.rb
@@ -10,9 +10,9 @@ class Scheduler::ShiftAssignmentsController < Scheduler::BaseController
     when 'mine'
       if controller.params[:person_id]
         controller.authorize! :read, Scheduler::ShiftAssignment.new(person_id: controller.params[:person_id])
-        scope.where{person_id == my{controller.params[:person_id]}}
+        scope.where(person_id: controller.params[:person_id])
       else
-        scope.where{person_id == my{controller.current_user.id}}
+        scope.where(person_id: controller.current_user.id)
       end
     when 'all'
       controller.authorize! :read_all_shifts, Scheduler::ShiftAssignment
@@ -65,8 +65,9 @@ class Scheduler::ShiftAssignmentsController < Scheduler::BaseController
 
   def collection
     @shift_assignments ||= apply_scopes(super).order(:date).left_outer_joins(:person)
-                  .where{(person.region_id == my{current_region}) & (date <= my{current_region.time_zone.today + 30})}
-                  .includes(:person, shift_time: :region, shift: :shift_territory, person: [:shift_territories, :region]).uniq
+                  .where(person: { region_id: current_region})
+                  .where('date <= ?', current_region.time_zone.today + 30)
+                  .includes(:person, shift_group: :region, shift: :shift_territory, person: [:shift_territories, :region]).uniq
   end
 
   helper_method :grouped_collection

app/mailers/scheduler/directline_mailer.rb
@@ -45,7 +45,9 @@ class Scheduler::DirectlineMailer < ActionMailer::Base
     all_groups = Scheduler::ShiftTime.for_region(region).order(:start_offset).to_a
     daily_groups = all_groups.select{ |grp| grp.period == 'daily' && dispatch_group_ids.include?(grp.id) }
     
-    all_assignments = Scheduler::ShiftAssignment.where{shift_id.in(dispatch_shifts)}.normalized_date_on_or_after(start_date).where{date <= end_date}.group_by{|sa| [sa.date, sa.shift_id, sa.shift_time_id]}
+    all_assignments = Scheduler::ShiftAssignment.where(shift_id: dispatch_shifts)
+      .normalized_date_on_or_after(start_date)
+      .where('date <= ?', end_date).group_by{|sa| [sa.date, sa.shift_id, sa.shift_time_id]}
 
     (start_date..end_date).each do |date|
       daily_groups.each do |daily_group|

app/models/scheduler/notification_setting.rb
@@ -5,25 +5,25 @@ class Scheduler::NotificationSetting < ApplicationRecord
 
   def self.needs_daily_email(region); needs_daily(region, :email); end
   def self.needs_daily_sms(region);   needs_daily(region, :sms); end
-  def self.needs_daily_swap(region);  for_region(region).with_active_person.where{email_all_swaps_daily == true}; end
+  def self.needs_daily_swap(region);  for_region(region).with_active_person.where(email_all_swaps_daily: true); end
 
   def self.needs_daily region, method
     now = region.time_zone.now
     midnight = now.at_beginning_of_day
     offset = now.seconds_since_midnight
-    for_region(region).with_active_person.where{(__send__(:"#{method}_all_shifts_at") != nil) & 
-                                                  (__send__(:"#{method}_all_shifts_at") <= offset) & 
-                                                  ( (__send__(:"last_all_shifts_#{method}") == nil) | 
-                                                    (__send__(:"last_all_shifts_#{method}") < midnight)
-                                                  )}.readonly(false)
+    for_region(region).with_active_person
+      .where("#{__send__(:"#{method}_all_shifts_at")} IS NOT NULL")
+      .where("#{__send__(:"#{method}_all_shifts_at")} <= ?", offset)
+      .where("#{__send__(:"last_all_shifts_#{method}")} IS NULL OR #{__send__(:"last_all_shifts_#{method}")} < ?", midnight)
+      .readonly(false)
   end
 
   def self.with_active_person
-    joins(:person).where{person.vc_is_active == true}
+    joins(:person).where(person: { vc_is_active: true })
   end
 
   def self.for_region region
-    joins(:person).where{person.region_id == region.id}
+    joins(:person).where(person: { region_id: region.id })
   end
 
   before_create :set_calendar_api_token
@@ -36,7 +36,9 @@ class Scheduler::NotificationSetting < ApplicationRecord
   end
 
   def self.admins_to_notify_swap(shift_assignment, ignore=[])
-    Roster::Person.joins(:notification_setting).in_shift_territory(shift_assignment.shift.shift_territory).where{(notification_setting.email_all_swaps == true) & (id.not_in ignore)}.to_a
+    Roster::Person.joins(:notification_setting).in_shift_territory(shift_assignment.shift.shift_territory)
+      .where(notification_setting: { email_all_swaps: true })
+      .where.not(id: ignore).to_a
   end
 
   def allow_sms_at? time

app/models/scheduler/shift.rb
@@ -72,20 +72,23 @@ class Scheduler::Shift < ApplicationRecord
   end
 
   scope :for_region, -> region {
-    joins(:shift_territory).where{shift_territory.region_id == region}
+    joins(:shift_territory).where(shift_territory: { region_id: region })
   }
   scope :active_on_day, -> date {
     #Todo: check day of week here
-    where{((shift_begins == nil) | (shift_begins <= date) & ((shift_ends == nil) | (shift_ends > date)))}
+    where('shift_begins IS NULL OR shift_begins <= ? AND (shift_ends IS NULL OR shift_ends > ?)', date, date)
   }
   scope :for_shift_territories, -> shift_territories {
-    where{shift_territory_id.in(shift_territories)}
+    where(shift_territory_id: shift_territories)
   }
   scope :can_be_taken_by, -> person {
-    where{((ignore_shift_territory == true) | shift_territory_id.in(person.shift_territory_ids))}.joins(:positions).where{positions.id.in(person.position_ids)}.uniq
+    where(ignore_shift_territory: true).or(where(shift_territory_id: person.shift_territory_ids))
+      .joins(:positions)
+      .where(positions: { id: person.position_ids })
+      .uniq
   }
   scope :for_groups, -> groups {
-    joins(:shift_times).where{shift_times.id.in groups}
+    joins(:shift_times).where(shift_times: { id: groups })
   }
 
   def can_be_taken_by?(person)
@@ -118,7 +121,7 @@ class Scheduler::Shift < ApplicationRecord
       end
     end
 
-    arr = Scheduler::ShiftAssignment.where{shift_id.in(shifts.to_a) & date.in(month..(month.at_end_of_month))}
+    arr = Scheduler::ShiftAssignment.where(shift_id: shifts.to_a, date: month..(month.at_end_of_month))
                               .select{[count(id).as(:count), shift_id, shift_time_id, date]}.group{[shift_id, shift_time_id, date]}.reduce(arr) do |hash, ass|
       shift = shifts_by_id[ass.shift_id]
       group = groups_by_id[ass.shift_time_id]