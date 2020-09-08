redcross/dcsops

Rails 5 upgrade: rspec updates
This updates rspec-rails to the latest version (compatibility from
Rails 5 to 6) and updates controller specs to reflect some API changes:

- params must be passed under a named `params` arg
- dedicated XHR method is deprecated and replaced with `xhr: true` arg
- controller testing has been extracted into an external gem
  (rails-controller-testing)
- it's not necessary to specify `rspec` directly in the Gemfile

spec/controllers/incidents/cas_link_controller_spec.rb
@@ -13,7 +13,7 @@ describe Incidents::CasLinkController, :type => :controller do
 
     inc.link_to_cas_incident(cas2)
 
-    get :index, chapter_id: inc.chapter.to_param
+    get :index, params: { chapter_id: inc.chapter.to_param }
 
     expect(response).to be_success
     expect(controller.send(:collection)).to match_array([cas])
@@ -23,7 +23,7 @@ describe Incidents::CasLinkController, :type => :controller do
     cas = FactoryGirl.create :cas_incident, chapter: @person.chapter
     inc = FactoryGirl.create :incident, chapter: @person.chapter
 
-    post :link, id: cas.to_param, incident_id: inc.id, chapter_id: inc.chapter.to_param
+    post :link, params: { id: cas.to_param, incident_id: inc.id, chapter_id: inc.chapter.to_param }
     expect(response).to be_redirect
     expect(flash[:info]).not_to be_empty
 
@@ -37,7 +37,7 @@ describe Incidents::CasLinkController, :type => :controller do
 
     inc.link_to_cas_incident(cas2)
     expect {
-      post :link, id: cas2.to_param, incident_id: inc2.id, chapter_id: inc.chapter.to_param
+      post :link, params: { id: cas2.to_param, incident_id: inc2.id, chapter_id: inc.chapter.to_param }
       expect(response).to be_redirect
       expect(flash[:error]).not_to be_empty
     }.to_not change{inc.reload.cas_event_number}
@@ -47,7 +47,7 @@ describe Incidents::CasLinkController, :type => :controller do
     cas = FactoryGirl.create :cas_incident, chapter: @person.chapter
 
     expect {
-      post :ignore, id: cas.to_param, chapter_id: @person.chapter.to_param
+      post :ignore, params: { id: cas.to_param, chapter_id: @person.chapter.to_param }
     }.to change{cas.reload.ignore_incident}.to(true)
 
     expect(response).to be_redirect
@@ -63,7 +63,7 @@ describe Incidents::CasLinkController, :type => :controller do
              city: Faker::Address.city, district: Faker::Address.city, zip: Faker::Address.zip_code, state: Faker::Address.state)
 
     expect {
-      post :promote, id: cas.to_param, commit: 'Promote to Incident', chapter_id: cas.chapter.to_param
+      post :promote, params: { id: cas.to_param, commit: 'Promote to Incident', chapter_id: cas.chapter.to_param }
       expect(response).to be_redirect
       expect(flash[:info]).not_to be_empty
 

spec/controllers/incidents/dat_incidents_controller_spec.rb
@@ -26,14 +26,14 @@ describe Incidents::DatIncidentsController, :type => :controller do
       incident = FactoryGirl.create :incident, chapter: @person.chapter
       dat = FactoryGirl.create :dat_incident, incident: incident
 
-      get :new, incident_id: incident.to_param, chapter_id: incident.chapter.to_param
+      get :new, params: { incident_id: incident.to_param, chapter_id: incident.chapter.to_param }
 
       expect(response).to redirect_to(action: :edit, incident_id: incident.to_param)
     end
 
     it "should render under an incident" do
       incident = FactoryGirl.create :incident, chapter: @person.chapter
-      get :new, incident_id: incident.to_param, chapter_id: incident.chapter.to_param
+      get :new, params: { incident_id: incident.to_param, chapter_id: incident.chapter.to_param }
       expect(response).to be_success
     end
 
@@ -48,11 +48,11 @@ describe Incidents::DatIncidentsController, :type => :controller do
       @dat = FactoryGirl.create :dat_incident, incident: @incident
     end
     it "should render under an incident" do
-      get :edit, incident_id: @incident.to_param, chapter_id: @incident.chapter.to_param
+      get :edit, params: { incident_id: @incident.to_param, chapter_id: @incident.chapter.to_param }
     end
     it "should not render standalone" do
       expect {
-        get :edit, id: @dat.to_param, chapter_id: @incident.chapter.to_param
+        get :edit, params: { id: @dat.to_param, chapter_id: @incident.chapter.to_param }
       }.to raise_error
     end
   end
@@ -82,21 +82,21 @@ describe Incidents::DatIncidentsController, :type => :controller do
 
     it "should allow creating" do
       expect {
-        post :create, incident_id: @incident.to_param, chapter_id: @incident.chapter.to_param, incidents_dat_incident: create_attrs
+        post :create, params: { incident_id: @incident.to_param, chapter_id: @incident.chapter.to_param, incidents_dat_incident: create_attrs }
         expect(response).to redirect_to(incidents_chapter_incident_path(@incident.chapter, @incident))
       }.to change(Incidents::DatIncident, :count).by(1)
     end
     it "should not change incident attributes" do
       create_attrs[:incident_attributes].merge!( {:incident_number => "15-555"})
       expect {
-        post :create, incident_id: @incident.to_param, chapter_id: @incident.chapter.to_param, incidents_dat_incident: create_attrs
+        post :create, params: { incident_id: @incident.to_param, chapter_id: @incident.chapter.to_param, incidents_dat_incident: create_attrs }
         expect(response).to redirect_to(incidents_chapter_incident_path(@incident.chapter, @incident))
       }.to_not change{@incident.reload.incident_number}
     end
     xit "should notify the report was filed" do
       expect(Incidents::Notifications::Notification).to receive(:create_for_event).with(anything, 'incident_report_filed', {is_new: true})
       create_attrs[:incident_attributes][:status] = 'closed'
-      post :create, incident_id: @incident.to_param, chapter_id: @incident.chapter.to_param, incidents_dat_incident: create_attrs
+      post :create, params: { incident_id: @incident.to_param, chapter_id: @incident.chapter.to_param, incidents_dat_incident: create_attrs }
     end
   end
 
@@ -108,7 +108,7 @@ describe Incidents::DatIncidentsController, :type => :controller do
 
     it "should notify the report was filed" do
       expect(Incidents::Notifications::Notification).to receive(:create_for_event).with(anything, 'incident_report_filed', {is_new: false})
-      put :update, incident_id: @incident.to_param, chapter_id: @incident.chapter.to_param, incidents_dat_incident: {incident_attributes: {num_adults: 3}}
+      put :update, params: { incident_id: @incident.to_param, chapter_id: @incident.chapter.to_param, incidents_dat_incident: {incident_attributes: {num_adults: 3}} }
       expect(response).to redirect_to(incidents_chapter_incident_path(@incident.chapter, @incident))
     end
   end

spec/controllers/incidents/responders_controller_spec.rb
@@ -43,12 +43,12 @@ describe Incidents::RespondersController, :type => :controller do
     describe "with valid params" do
       it "creates a new Incidents::Responder" do
         expect {
-          post :create, {:incidents_responder_assignment => valid_attributes, incident_id: incident.to_param, chapter_id: incident.chapter.to_param}, valid_session
+          post :create, params: { :incidents_responder_assignment => valid_attributes, incident_id: incident.to_param, chapter_id: incident.chapter.to_param }, session: valid_session
         }.to change(Incidents::ResponderAssignment, :count).by(1)
       end
 
       it "redirects to index" do
-        post :create, {:incidents_responder_assignment => valid_attributes, incident_id: incident.to_param, chapter_id: incident.chapter.to_param}, valid_session
+        post :create, params: { :incidents_responder_assignment => valid_attributes, incident_id: incident.to_param, chapter_id: incident.chapter.to_param }, session: valid_session
         expect(response).to redirect_to(incidents_chapter_incident_responders_url(incident.chapter, incident))
       end
 
@@ -58,26 +58,26 @@ describe Incidents::RespondersController, :type => :controller do
         Bitly.stub(client: double(:shorten => double(short_url: "https://short.url")))
         expect(client_stub).to receive(:send_message).with(an_instance_of(Incidents::ResponderMessage))
         expect(Incidents::RespondersMailer).to receive(:assign_email).and_return(double deliver: true)
-        post :create, {:incidents_responder_assignment => valid_attributes.merge(role: 'team_lead'), incident_id: incident.to_param, chapter_id: incident.chapter.to_param, send_assignment_sms: true, send_assignment_email: true}, valid_session
+        post :create, params: { :incidents_responder_assignment => valid_attributes.merge(role: 'team_lead'), incident_id: incident.to_param, chapter_id: incident.chapter.to_param, send_assignment_sms: true, send_assignment_email: true }, session: valid_session
       end
 
       it "triggers the assignment mailers with a non-responding role" do
         expect(Incidents::RespondersMailer).not_to receive(:assign_sms)
         expect(Incidents::RespondersMailer).not_to receive(:assign_email)
-        post :create, {:incidents_responder_assignment => valid_attributes.merge(role: 'not_available'), incident_id: incident.to_param, chapter_id: incident.chapter.to_param, send_assignment_sms: true, send_assignment_email: true}, valid_session
+        post :create, params: { :incidents_responder_assignment => valid_attributes.merge(role: 'not_available'), incident_id: incident.to_param, chapter_id: incident.chapter.to_param, send_assignment_sms: true, send_assignment_email: true }, session: valid_session
       end
     end
   end
 
   describe "GET index" do
     it "should succeed" do
-      get :index, {incident_id: incident.to_param, chapter_id: incident.chapter.to_param}
+      get :index, params: { incident_id: incident.to_param, chapter_id: incident.chapter.to_param }
       expect(response).to be_success
     end
 
     it "should set the flash if incident doesn't have a location" do
       incident.update_attributes lat: nil, lng: nil
-      get :index, {incident_id: incident.to_param, chapter_id: incident.chapter.to_param}
+      get :index, params: { incident_id: incident.to_param, chapter_id: incident.chapter.to_param }
       expect(response).to be_success
       expect(flash.now[:error]).not_to be_empty
     end
@@ -85,14 +85,14 @@ describe Incidents::RespondersController, :type => :controller do
 
   describe "GET new" do
     it "should succeed" do
-      get :new, {incident_id: incident.to_param, chapter_id: incident.chapter.to_param}
+      get :new, params: { incident_id: incident.to_param, chapter_id: incident.chapter.to_param }
       expect(response).to be_success
 
       expect(controller.send(:person)).to eq(nil)
     end
 
     it "should assign the person if given" do
-      get :new, {incident_id: incident.to_param, chapter_id: incident.chapter.to_param, person_id: person.id}
+      get :new, params: { incident_id: incident.to_param, chapter_id: incident.chapter.to_param, person_id: person.id }
       expect(response).to be_success
 
       expect(controller.send(:person)).to eq(person)

spec/controllers/scheduler/flex_schedules_spec.rb
@@ -16,7 +16,7 @@ describe Scheduler::FlexSchedulesController, :type => :controller do
     end
 
     it "should filter by county" do
-      get :index, county: @person.county_ids.first
+      get :index, params: { county: @person.county_ids.first }
       expect(response).to be_success
     end
 
@@ -29,12 +29,12 @@ describe Scheduler::FlexSchedulesController, :type => :controller do
 
   describe "#show" do
     it "should render" do
-      get :show, id: @person.id
+      get :show, params: { id: @person.id }
       expect(response).to be_success
     end
 
     it "should allow updating" do
-      put :update, id: @person.id, scheduler_flex_schedule: {available_monday_day: true}, format: 'json'
+      put :update, params: { id: @person.id, scheduler_flex_schedule: {available_monday_day: true}, format: 'json' }
       expect(response).to be_success
       expect(@sched.reload.available_monday_day).to eq true
     end

clix-dev-llc/arcdata

spec/controllers/incidents/cas_link_controller_spec.rb
@@ -13,7 +13,7 @@ describe Incidents::CasLinkController, :type => :controller do
 
     inc.link_to_cas_incident(cas2)
 
-    get :index, region_id: inc.region.to_param
+    get :index, params: { region_id: inc.region.to_param }
 
     expect(response).to be_success
     expect(controller.send(:collection)).to match_array([cas])
@@ -23,7 +23,7 @@ describe Incidents::CasLinkController, :type => :controller do
     cas = FactoryGirl.create :cas_incident, region: @person.region
     inc = FactoryGirl.create :incident, region: @person.region
 
-    post :link, id: cas.to_param, incident_id: inc.id, region_id: inc.region.to_param
+    post :link, params: { id: cas.to_param, incident_id: inc.id, region_id: inc.region.to_param }
     expect(response).to be_redirect
     expect(flash[:info]).not_to be_empty
 
@@ -37,7 +37,7 @@ describe Incidents::CasLinkController, :type => :controller do
 
     inc.link_to_cas_incident(cas2)
     expect {
-      post :link, id: cas2.to_param, incident_id: inc2.id, region_id: inc.region.to_param
+      post :link, params: { id: cas2.to_param, incident_id: inc2.id, region_id: inc.region.to_param }
       expect(response).to be_redirect
       expect(flash[:error]).not_to be_empty
     }.to_not change{inc.reload.cas_event_number}
@@ -47,7 +47,7 @@ describe Incidents::CasLinkController, :type => :controller do
     cas = FactoryGirl.create :cas_incident, region: @person.region
 
     expect {
-      post :ignore, id: cas.to_param, region_id: @person.region.to_param
+      post :ignore, params: { id: cas.to_param, region_id: @person.region.to_param }
     }.to change{cas.reload.ignore_incident}.to(true)
 
     expect(response).to be_redirect
@@ -63,7 +63,7 @@ describe Incidents::CasLinkController, :type => :controller do
              city: Faker::Address.city, district: Faker::Address.city, zip: Faker::Address.zip_code, state: Faker::Address.state)
 
     expect {
-      post :promote, id: cas.to_param, commit: 'Promote to Incident', region_id: cas.region.to_param
+      post :promote, params: { id: cas.to_param, commit: 'Promote to Incident', region_id: cas.region.to_param }
       expect(response).to be_redirect
       expect(flash[:info]).not_to be_empty
 

spec/controllers/incidents/dat_incidents_controller_spec.rb
@@ -26,14 +26,14 @@ describe Incidents::DatIncidentsController, :type => :controller do
       incident = FactoryGirl.create :incident, region: @person.region
       dat = FactoryGirl.create :dat_incident, incident: incident
 
-      get :new, incident_id: incident.to_param, region_id: incident.region.to_param
+      get :new, params: { incident_id: incident.to_param, region_id: incident.region.to_param }
 
       expect(response).to redirect_to(action: :edit, incident_id: incident.to_param)
     end
 
     it "should render under an incident" do
       incident = FactoryGirl.create :incident, region: @person.region
-      get :new, incident_id: incident.to_param, region_id: incident.region.to_param
+      get :new, params: { incident_id: incident.to_param, region_id: incident.region.to_param }
       expect(response).to be_success
     end
 
@@ -48,11 +48,11 @@ describe Incidents::DatIncidentsController, :type => :controller do
       @dat = FactoryGirl.create :dat_incident, incident: @incident
     end
     it "should render under an incident" do
-      get :edit, incident_id: @incident.to_param, region_id: @incident.region.to_param
+      get :edit, params: { incident_id: @incident.to_param, region_id: @incident.region.to_param }
     end
     it "should not render standalone" do
       expect {
-        get :edit, id: @dat.to_param, region_id: @incident.region.to_param
+        get :edit, params: { id: @dat.to_param, region_id: @incident.region.to_param }
       }.to raise_error
     end
   end
@@ -82,21 +82,21 @@ describe Incidents::DatIncidentsController, :type => :controller do
 
     it "should allow creating" do
       expect {
-        post :create, incident_id: @incident.to_param, region_id: @incident.region.to_param, incidents_dat_incident: create_attrs
+        post :create, params: { incident_id: @incident.to_param, region_id: @incident.region.to_param, incidents_dat_incident: create_attrs }
         expect(response).to redirect_to(incidents_region_incident_path(@incident.region, @incident))
       }.to change(Incidents::DatIncident, :count).by(1)
     end
     it "should not change incident attributes" do
       create_attrs[:incident_attributes].merge!( {:incident_number => "15-555"})
       expect {
-        post :create, incident_id: @incident.to_param, region_id: @incident.region.to_param, incidents_dat_incident: create_attrs
+        post :create, params: { incident_id: @incident.to_param, region_id: @incident.region.to_param, incidents_dat_incident: create_attrs }
         expect(response).to redirect_to(incidents_region_incident_path(@incident.region, @incident))
       }.to_not change{@incident.reload.incident_number}
     end
     xit "should notify the report was filed" do
       expect(Incidents::Notifications::Notification).to receive(:create_for_event).with(anything, 'incident_report_filed', {is_new: true})
       create_attrs[:incident_attributes][:status] = 'closed'
-      post :create, incident_id: @incident.to_param, region_id: @incident.region.to_param, incidents_dat_incident: create_attrs
+      post :create, params: { incident_id: @incident.to_param, region_id: @incident.region.to_param, incidents_dat_incident: create_attrs }
     end
   end
 
@@ -108,7 +108,7 @@ describe Incidents::DatIncidentsController, :type => :controller do
 
     it "should notify the report was filed" do
       expect(Incidents::Notifications::Notification).to receive(:create_for_event).with(anything, 'incident_report_filed', {is_new: false})
-      put :update, incident_id: @incident.to_param, region_id: @incident.region.to_param, incidents_dat_incident: {incident_attributes: {num_adults: 3}}
+      put :update, params: { incident_id: @incident.to_param, region_id: @incident.region.to_param, incidents_dat_incident: {incident_attributes: {num_adults: 3}} }
       expect(response).to redirect_to(incidents_region_incident_path(@incident.region, @incident))
     end
   end

spec/controllers/incidents/responders_controller_spec.rb
@@ -43,12 +43,12 @@ describe Incidents::RespondersController, :type => :controller do
     describe "with valid params" do
       it "creates a new Incidents::Responder" do
         expect {
-          post :create, {:incidents_responder_assignment => valid_attributes, incident_id: incident.to_param, region_id: incident.region.to_param}, valid_session
+          post :create, params: { :incidents_responder_assignment => valid_attributes, incident_id: incident.to_param, region_id: incident.region.to_param }, session: valid_session
         }.to change(Incidents::ResponderAssignment, :count).by(1)
       end
 
       it "redirects to index" do
-        post :create, {:incidents_responder_assignment => valid_attributes, incident_id: incident.to_param, region_id: incident.region.to_param}, valid_session
+        post :create, params: { :incidents_responder_assignment => valid_attributes, incident_id: incident.to_param, region_id: incident.region.to_param }, session: valid_session
         expect(response).to redirect_to(incidents_region_incident_responders_url(incident.region, incident))
       end
 
@@ -58,26 +58,26 @@ describe Incidents::RespondersController, :type => :controller do
         Bitly.stub(client: double(:shorten => double(short_url: "https://short.url")))
         expect(client_stub).to receive(:send_message).with(an_instance_of(Incidents::ResponderMessage))
         expect(Incidents::RespondersMailer).to receive(:assign_email).and_return(double deliver: true)
-        post :create, {:incidents_responder_assignment => valid_attributes.merge(role: 'team_lead'), incident_id: incident.to_param, region_id: incident.region.to_param, send_assignment_sms: true, send_assignment_email: true}, valid_session
+        post :create, params: { :incidents_responder_assignment => valid_attributes.merge(role: 'team_lead'), incident_id: incident.to_param, region_id: incident.region.to_param, send_assignment_sms: true, send_assignment_email: true }, session: valid_session
       end
 
       it "triggers the assignment mailers with a non-responding role" do
         expect(Incidents::RespondersMailer).not_to receive(:assign_sms)
         expect(Incidents::RespondersMailer).not_to receive(:assign_email)
-        post :create, {:incidents_responder_assignment => valid_attributes.merge(role: 'not_available'), incident_id: incident.to_param, region_id: incident.region.to_param, send_assignment_sms: true, send_assignment_email: true}, valid_session
+        post :create, params: { :incidents_responder_assignment => valid_attributes.merge(role: 'not_available'), incident_id: incident.to_param, region_id: incident.region.to_param, send_assignment_sms: true, send_assignment_email: true }, session: valid_session
       end
     end
   end
 
   describe "GET index" do
     it "should succeed" do
-      get :index, {incident_id: incident.to_param, region_id: incident.region.to_param}
+      get :index, params: { incident_id: incident.to_param, region_id: incident.region.to_param }
       expect(response).to be_success
     end
 
     it "should set the flash if incident doesn't have a location" do
       incident.update_attributes lat: nil, lng: nil
-      get :index, {incident_id: incident.to_param, region_id: incident.region.to_param}
+      get :index, params: { incident_id: incident.to_param, region_id: incident.region.to_param }
       expect(response).to be_success
       expect(flash.now[:error]).not_to be_empty
     end
@@ -85,14 +85,14 @@ describe Incidents::RespondersController, :type => :controller do
 
   describe "GET new" do
     it "should succeed" do
-      get :new, {incident_id: incident.to_param, region_id: incident.region.to_param}
+      get :new, params: { incident_id: incident.to_param, region_id: incident.region.to_param }
       expect(response).to be_success
 
       expect(controller.send(:person)).to eq(nil)
     end
 
     it "should assign the person if given" do
-      get :new, {incident_id: incident.to_param, region_id: incident.region.to_param, person_id: person.id}
+      get :new, params: { incident_id: incident.to_param, region_id: incident.region.to_param, person_id: person.id }
       expect(response).to be_success
 
       expect(controller.send(:person)).to eq(person)

spec/controllers/scheduler/flex_schedules_spec.rb
@@ -16,7 +16,7 @@ describe Scheduler::FlexSchedulesController, :type => :controller do
     end
 
     it "should filter by shift territory" do
-      get :index, shift_territory: @person.shift_territory_ids.first
+      get :index, params: { shift_territory: @person.shift_territory_ids.first }
       expect(response).to be_success
     end
 
@@ -29,12 +29,12 @@ describe Scheduler::FlexSchedulesController, :type => :controller do
 
   describe "#show" do
     it "should render" do
-      get :show, id: @person.id
+      get :show, params: { id: @person.id }
       expect(response).to be_success
     end
 
     it "should allow updating" do
-      put :update, id: @person.id, scheduler_flex_schedule: {available_monday_day: true}, format: 'json'
+      put :update, params: { id: @person.id, scheduler_flex_schedule: {available_monday_day: true}, format: 'json' }
       expect(response).to be_success
       expect(@sched.reload.available_monday_day).to eq true
     end