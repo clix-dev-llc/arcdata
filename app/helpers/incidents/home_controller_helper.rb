redcross/dcsops

Unsqueel: misc additional de-squeeling
This is the result of arbitrarily picking a feature spec
(global_log_spec.rb) and replacing the squeel usage required to execute
it (though I never did get it passing, it is at least running now).

app/helpers/incidents/home_controller_helper.rb
@@ -5,7 +5,7 @@ module Incidents::HomeControllerHelper
   end
 
   def stats(date_range)
-    stats = scope.incidents.where{date.in(date_range)}.valid
+    stats = scope.incidents.where(date: date_range).valid
     stats = stats.with_status 'closed' if current_chapter.incidents_report_editable
     stats.incident_stats
   end
@@ -44,7 +44,7 @@ module Incidents::HomeControllerHelper
   end
 
   def map_json_for(incidents)
-    incidents.joins{chapter}.pluck(:incident_number, :lat, :lng, :num_adults, :num_children, :status, "roster_chapters.url_slug").map do |inc|
+    incidents.joins(:chapter).pluck(:incident_number, :lat, :lng, :num_adults, :num_children, :status, "roster_chapters.url_slug").map do |inc|
       {id: inc[0], lat: inc[1], lng: inc[2], clients: [inc[3], inc[4]].compact.sum, status: inc[5], url: incidents_chapter_incident_path(inc[6], inc[0])}
     end
   end

app/models/homepage_link.rb
@@ -9,7 +9,7 @@ class HomepageLink < ApplicationRecord
     file.file? && file.expiring_url || url
   end
 
-  scope :for_chapter, -> (chapter) { where{(chapter_id == nil) | (chapter_id == chapter)}}
+  scope :for_chapter, -> (chapter) { where(chapter_id: nil).or(where(chapter: chapter)) }
 
   class Role < ApplicationRecord
     self.table_name = :homepage_link_roles

app/models/incidents/scope.rb
@@ -6,7 +6,7 @@ class Incidents::Scope < ApplicationRecord
   has_many :report_subscriptions, class_name: 'Incidents::ReportSubscription'
 
   def self.for_chapter chapter
-    where{chapter_id == chapter}.first!
+    where(chapter: chapter).first!
   end
 
   def self.including_chapter chapter_ids

clix-dev-llc/arcdata

app/helpers/incidents/home_controller_helper.rb
@@ -5,7 +5,7 @@ module Incidents::HomeControllerHelper
   end
 
   def stats(date_range)
-    stats = scope.incidents.where{date.in(date_range)}.valid
+    stats = scope.incidents.where(date: date_range).valid
     stats = stats.with_status 'closed' if current_region.incidents_report_editable
     stats.incident_stats
   end
@@ -32,7 +32,7 @@ module Incidents::HomeControllerHelper
   end
 
   def map_json_for(incidents)
-    incidents.joins{region}.pluck(:incident_number, :lat, :lng, :num_adults, :num_children, :status, "roster_regions.url_slug").map do |inc|
+    incidents.joins(:region).pluck(:incident_number, :lat, :lng, :num_adults, :num_children, :status, "roster_regions.url_slug").map do |inc|
       {id: inc[0], lat: inc[1], lng: inc[2], clients: [inc[3], inc[4]].compact.sum, status: inc[5], url: incidents_region_incident_path(inc[6], inc[0])}
     end
   end

app/models/homepage_link.rb
@@ -9,7 +9,7 @@ class HomepageLink < ApplicationRecord
     file.file? && file.expiring_url || url
   end
 
-  scope :for_region, -> (region) { where{(region_id == nil) | (region_id == region)}}
+  scope :for_region, -> (region) { where(region_id: nil).or(where(region: region)) }
 
   class Role < ApplicationRecord
     self.table_name = :homepage_link_roles

app/models/incidents/scope.rb
@@ -6,7 +6,7 @@ class Incidents::Scope < ApplicationRecord
   has_many :report_subscriptions, class_name: 'Incidents::ReportSubscription'
 
   def self.for_region region
-    where{region_id == region}.first!
+    where(region: region).first!
   end
 
   def self.including_region region_ids